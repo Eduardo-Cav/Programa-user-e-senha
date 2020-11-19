7807408873c022c1ef6bbc80588a7b15c4ccc05f
Eduardo <eduzika2903@hotmail.com>
2020-11-19 20:11:08 -0300

    Programa user

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Programa_user_e_senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha;
+            string usuario;
+            do{
+            Console.WriteLine("Digite seu nome de usuário: ");
+            usuario = Console.ReadLine();
+
+            Console.WriteLine("Digite sua senha: ");
+            senha = Console.ReadLine();
+
+            if(usuario == senha){
+                Console.WriteLine("A senha não pode ser igual ao nome de usuário");
+            }
+
+            }while(usuario == senha);
+
+            Console.WriteLine("Login concluído");
+        }
+        
+    }
+}
